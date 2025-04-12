3aafccf0e6ec3cec5426cb88527d37a5b7c18f4d
javlonbek556 <javlonzdess@gmail.com>
2025-04-12 09:02:01 +0500

    relation connection models here

---
+++ app/Models/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+"name",
+"comment",
+
+"commentable_id",
+"commentable_type",    ];
+
+public function commentable()
+{
+    return $this->morphTo(User::class);
+
+}
+}
+
+
